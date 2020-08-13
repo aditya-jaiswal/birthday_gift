c15065e11e7a97c4576c3d956ad89907a1bf220f
Aditya <adityajaiswal2807@gmail.com>
2020-08-13 13:13:50 +0530

    Updates

---
+++ precache-manifest.75435d5eaf5fe18e338e483445cdf577.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "526b8fcd431c693efe5283309b2bdf95",
+    "url": "/birthday_gift/index.html"
+  },
+  {
+    "revision": "1b65a63fe9ad17b25302",
+    "url": "/birthday_gift/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0fe698daa6348f7ad5f3",
+    "url": "/birthday_gift/static/js/2.fea7bcba.chunk.js"
+  },
+  {
+    "revision": "6dea14eab83053243865ab6f2019f727",
+    "url": "/birthday_gift/static/js/2.fea7bcba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b65a63fe9ad17b25302",
+    "url": "/birthday_gift/static/js/main.6a0a22d2.chunk.js"
+  },
+  {
+    "revision": "db96665e2bd69342b7d9",
+    "url": "/birthday_gift/static/js/runtime-main.af4177ef.js"
+  }
+]);
