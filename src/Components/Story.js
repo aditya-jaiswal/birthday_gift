a314adc37e29d82a8ac33a1e15e937bda6a3848d
Aditya <adityajaiswal2807@gmail.com>
2020-08-17 23:57:37 +0530

    Adding the demo changes for story, and gallery

--- src/Components/Story.js
+++ src/Components/Story.js
@@ -55,76 +55,261 @@ class Story extends Component {
               contentArrowStyle={{
                 borderRight: "7px solid  rgb(33, 150, 243)",
               }}
-              date={<p style={{ fontSize: "25px" }}>2011 - present</p>}
+              date={<p style={{ color: "#000", fontSize: "20px" }}>1998</p>}
               iconStyle={{
                 background: "rgb(33, 150, 243)",
-                size: "30px",
+                size: "20px",
                 color: "#fff",
               }}
-              icon={<i className="fa fa-download"></i>}
+              // icon={<i className="fa fa-download"></i>}
             >
-              <img src="images/ayu2.jpg" />
+              <img src="images/ayu3.jpg" />
               <h3 className="vertical-timeline-element-title">
-                Creative Director
+                When the angel was born.
               </h3>
-              <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-              <p>
-                Creative Direction, User Experience, Visual Design, Project
-                Management, Team Leading
+              <p style={{ fontSize: "14px" }}>
+                The date was 18/08/1998 when my angel came to this world.
               </p>
             </VerticalTimelineElement>
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
-              date="2010 - 2011"
+              date={<p style={{ color: "#000", fontSize: "20px" }}>2003</p>}
               iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
               //   icon={}
             >
-              <h3 className="vertical-timeline-element-title">Art Director</h3>
-              <h4 className="vertical-timeline-element-subtitle">
-                San Francisco, CA
-              </h4>
-              <p>
-                Creative Direction, User Experience, Visual Design, SEO, Online
-                Marketing
+              <img src="images/ayu4.jpg" />
+              <h3 className="vertical-timeline-element-title">
+                Caring from childhood
+              </h3>
+              <p style={{ fontSize: "14px" }}>Always a caring person</p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              contentArrowStyle={{
+                borderRight: "7px solid  rgb(33, 150, 243)",
+              }}
+              date={
+                <p style={{ color: "#000", fontSize: "20px" }}>2003-2007</p>
+              }
+              iconStyle={{
+                background: "rgb(33, 150, 243)",
+                size: "20px",
+                color: "#fff",
+              }}
+              // icon={<i className="fa fa-download"></i>}
+            >
+              <img src="images/ayu5.jpg" />
+              <h3 className="vertical-timeline-element-title">
+                Always a winner
+              </h3>
+              <p style={{ fontSize: "14px" }}>
+                Always been in the competitive spirit
               </p>
             </VerticalTimelineElement>
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
-              date="2008 - 2010"
+              date={
+                <p style={{ color: "#000", fontSize: "20px" }}>2011-2019</p>
+              }
               iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-              // icon={<WorkIcon />}
+              //   icon={}
             >
-              <h3 className="vertical-timeline-element-title">Web Designer</h3>
-              <h4 className="vertical-timeline-element-subtitle">
-                Los Angeles, CA
-              </h4>
-              <p>User Experience, Visual Design</p>
+              <img src="images/ayu6.jpg" />
+              <h3 className="vertical-timeline-element-title">So Beautiful</h3>
+              <p style={{ fontSize: "14px" }}>You were born cute and hot</p>
             </VerticalTimelineElement>
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
-              date="2006 - 2008"
-              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-              // icon={<WorkIcon />}
+              contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              contentArrowStyle={{
+                borderRight: "7px solid  rgb(33, 150, 243)",
+              }}
+              date={<p style={{ color: "#000", fontSize: "20px" }}>May 2019</p>}
+              iconStyle={{
+                background: "rgb(33, 150, 243)",
+                size: "20px",
+                color: "#fff",
+              }}
+              // icon={<i className="fa fa-download"></i>}
             >
-              <h3 className="vertical-timeline-element-title">Web Designer</h3>
-              <h4 className="vertical-timeline-element-subtitle">
-                San Francisco, CA
-              </h4>
-              <p>User Experience, Visual Design</p>
+              <img src="images/ayu7.png" />
+              <h3 className="vertical-timeline-element-title">
+                The first time I found you
+              </h3>
+              <p style={{ fontSize: "14px" }}>
+                We sarted talking for the first time on this date.
+              </p>
             </VerticalTimelineElement>
             <VerticalTimelineElement
-              className="vertical-timeline-element--education"
-              date="April 2013"
-              iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
-              // icon={<SchoolIcon />}
+              className="vertical-timeline-element--work"
+              date={
+                <p style={{ color: "#000", fontSize: "20px" }}>July 2019</p>
+              }
+              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              //   icon={}
             >
+              <img src="images/ayu8.png" />
               <h3 className="vertical-timeline-element-title">
-                Content Marketing for Web, Mobile and Social Media
+                And then it continued
               </h3>
-              <h4 className="vertical-timeline-element-subtitle">
-                Online Course
-              </h4>
-              <p>Strategy, Social Media</p>
+              <p style={{ fontSize: "14px" }}>
+                We moved to Whatsapp and it became intense since then.
+              </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              contentArrowStyle={{
+                borderRight: "7px solid  rgb(33, 150, 243)",
+              }}
+              date={<p style={{ color: "#000", fontSize: "20px" }}>Oct 2019</p>}
+              iconStyle={{
+                background: "rgb(33, 150, 243)",
+                size: "20px",
+                color: "#fff",
+              }}
+              // icon={<i className="fa fa-download"></i>}
+            >
+              <img src="images/ayu9.jpeg" />
+              <h3 className="vertical-timeline-element-title">
+                The day I proposed you.
+              </h3>
+              <p style={{ fontSize: "14px" }}>
+                The date was 04/10/2019 when I proposed you. It was the same day
+                when i kissed you for the first time.
+              </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              date={<p style={{ color: "#000", fontSize: "20px" }}>Oct 2019</p>}
+              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              //   icon={}
+            >
+              <img src="images/ayu10.jpeg" />
+              <h3 className="vertical-timeline-element-title">
+                The day you proposed me
+              </h3>
+              <p style={{ fontSize: "14px" }}>
+                I never expected this. Thanks for always doing the unexpected
+                for me.
+              </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              contentArrowStyle={{
+                borderRight: "7px solid  rgb(33, 150, 243)",
+              }}
+              date={
+                <p style={{ color: "#000", fontSize: "20px" }}>November 2019</p>
+              }
+              iconStyle={{
+                background: "rgb(33, 150, 243)",
+                size: "20px",
+                color: "#fff",
+              }}
+              // icon={<i className="fa fa-download"></i>}
+            >
+              <img src="images/ayu11.jpg" />
+              <h3 className="vertical-timeline-element-title">
+                Fisrt month anniversary.
+              </h3>
+              <p style={{ fontSize: "14px" }}>
+                Thanks for taking me to new places. I promise to keep you happy
+                always.
+              </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              date={<p style={{ color: "#000", fontSize: "20px" }}>Nov 2019</p>}
+              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              //   icon={}
+            >
+              <img src="images/ayu12.jpeg" />
+              <h3 className="vertical-timeline-element-title">
+                When you became part of me
+              </h3>
+              <p style={{ fontSize: "14px" }}>
+                You are a part of me my doll and everything I have.
+              </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              contentArrowStyle={{
+                borderRight: "7px solid  rgb(33, 150, 243)",
+              }}
+              date={<p style={{ color: "#000", fontSize: "20px" }}>Nov 2019</p>}
+              iconStyle={{
+                background: "rgb(33, 150, 243)",
+                size: "20px",
+                color: "#fff",
+              }}
+              // icon={<i className="fa fa-download"></i>}
+            >
+              <img src="images/ayu13.jpg" />
+              <h3 className="vertical-timeline-element-title">Meet Tamasha</h3>
+              <p style={{ fontSize: "14px" }}>
+                Even though she is no more. A very thoughtful gift
+              </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              date={<p style={{ color: "#000", fontSize: "20px" }}>2003</p>}
+              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              //   icon={}
+            >
+              <img src="images/ayu14.jpg" />
+              <h3 className="vertical-timeline-element-title">
+                Two Masterchefs
+              </h3>
+              <p style={{ fontSize: "14px" }}>
+                Such a beautiful date it was. It was just us and pingu. BTW
+                pingu had the whole bed to himself
+              </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              contentArrowStyle={{
+                borderRight: "7px solid  rgb(33, 150, 243)",
+              }}
+              date={<p style={{ color: "#000", fontSize: "20px" }}>Feb 2020</p>}
+              iconStyle={{
+                background: "rgb(33, 150, 243)",
+                size: "20px",
+                color: "#fff",
+              }}
+              // icon={<i className="fa fa-download"></i>}
+            >
+              <img src="images/ayu15.jpg" />
+              <h3 className="vertical-timeline-element-title">
+                Our First Valentine
+              </h3>
+              <p style={{ fontSize: "14px" }}>
+                Thank you for making my first valentine day celebration so
+                special for me. I love you a lot.
+              </p>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              date={
+                <p style={{ color: "#000", fontSize: "20px" }}>
+                  Mar 2020 - Present
+                </p>
+              }
+              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              //   icon={}
+            >
+              <img src="images/ayu16.jpg" />
+              <h3 className="vertical-timeline-element-title">
+                Drunk and lovely people
+              </h3>
+              <p style={{ fontSize: "14px" }}>
+                We are perfect for each other. We are the best couple. I love
+                us. I love you.
+              </p>
             </VerticalTimelineElement>
           </VerticalTimeline>
         </div>
